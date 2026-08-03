88346df785fa7c531b047da12c3935cb1991a205
anasmahboubi123-del <anasmahboubi123@gmail.com>
2026-08-03 14:54:08 +0100

    إصلاح next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-const withPWA = require('next-pwa')({
+import withPWA from 'next-pwa'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-})
-
-module.exports = withPWA({
-  // إعداداتك الأخرى
-})
+  disable: process.env.NODE_ENV === 'development'
+})(nextConfig)
